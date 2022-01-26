c54d76fcf9b73659c2fed779d5928597eac7f7db
MSI <danhnt-2917>
2022-01-26 15:23:07 +0700

    push chapter 6

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  #config/routes.rb
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_page#home"
+    get "users/new"
+    get "static_page/home"
+    get "/help",  to: "static_page#help"
+    get "/about", to: "static_page#about"
+    get "/contact", to: "static_page#contact"
+    get "/signup", to: "users#new"
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
