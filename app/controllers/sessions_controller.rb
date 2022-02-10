62ca08a9dafc5595654ec6078dc46f5c0ac3767a
MSI <danhnt-2917>
2022-02-10 16:15:18 +0700

    push chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params.dig(:session, :email)&.downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      check_remember user
       redirect_to user
     else
       flash.now[:danger] = t ".invalid"
@@ -15,5 +16,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
+  end
+
+  private
+
+  def check_remember user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
