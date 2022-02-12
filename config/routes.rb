4b8c5712a9fd1f0f9b417665b704056daa49e375
MSI <danhnt-2917>
2022-02-13 00:56:28 +0700

    push chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    get "password/new", to: "password_resets#new"
+    get "password/edit", to: "password_resets#edit"
     resources :users, except: %i(new)
     resources :account_activations, only: :edit
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
